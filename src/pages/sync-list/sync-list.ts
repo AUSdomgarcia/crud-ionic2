84f0bce66bba6430830bf3a3cf0662ea94a752ed
bb_domgarcia <dom.garcia@nuworks.ph>
2017-06-19 19:11:05 +0800

    updates

--- src/pages/sync-list/sync-list.ts
+++ src/pages/sync-list/sync-list.ts
@@ -1,7 +1,6 @@
 import { Helpers, ApiSettings, NetworkSettings, SyncSettings } from '../../shared/shared';
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-// import * as moment from 'moment';
+import { IonicPage, LoadingController, NavController, NavParams, ToastController } from 'ionic-angular';
 import * as _ from 'lodash';
 /**
  * Generated class for the SyncListPage page.
@@ -17,27 +16,92 @@ import * as _ from 'lodash';
 export class SyncListPage {
 
   students;
+
+  initial_database_loader;
+
   notice;
+
+  latestTimeRef;
+
+  refresherRef;
+
   DO_NOTHING_HERE = {};
 
   constructor(public navCtrl: NavController, public navParams: NavParams,
               private syncSettings: SyncSettings,
               private apiSettings: ApiSettings,
               private networkSettings: NetworkSettings,
-              private helpers: Helpers) {}
+              private helpers: Helpers,
+              private toastCtrl: ToastController,
+              private loadingCtrl: LoadingController) {}
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SyncListPage');
     
-    if(this.networkSettings.isAvailable()){
+    if( ! this.networkSettings.isAvailable()){
       
       this.initialDBSync();
 
     } else {
       // TODO: 
       // At start, internet is required to sync initial database from the server to app.
+      alert('No internet.');
+
+      this.processOffline();
     }
-    
+  }
+
+  processOffline(){
+    // Try to view database if not empty
+    this.checkEmptyDatabase()
+    .then( (res: any) => {
+      if(res.rows.length !== 0){
+
+        this.populateStudents();
+
+      } else {
+        this.presentToast('Connection required.');
+      }
+    })
+    .catch( (err) => {
+      alert(JSON.stringify(err));
+    });
+  }
+
+  multiple_db_insert = (res: any) => {
+      let promises = [];
+      let query = `INSERT INTO students VALUES (?, ?, ?, ?, ?, ?)`;
+
+      _.map(res, (student, key) => {
+          promises.push( 
+            this.syncSettings.queryBuilder(query, 
+              [
+                student.name,
+                student.age,
+                student.information,
+                student.level,
+                student.created_at,
+                student.updated_at,
+              ])
+            );
+      });
+
+    return Promise.all(promises);
+  }
+
+  checkEmptyDatabase = () => {
+    let query = `SELECT * from students`;
+
+    let promise = new Promise( (resolve, reject) => {
+      this.syncSettings.queryBuilder(query, [])
+        .then( (res) => { 
+          resolve(res);
+        })
+        .catch( (err) => {
+          reject(err);
+        });
+    });
+    return promise;
   }
 
   initialDBSync(){
@@ -46,34 +110,22 @@ export class SyncListPage {
     | data from the server to local SQLite of the app
     | TODO: optimization
     */
-
-    const checkEmptyDatabase = () => {
-        let query = `SELECT * from students`;
-
-        let promise = new Promise( (resolve, reject) => {
-          this.syncSettings.queryBuilder(query, [])
-            .then( (res) => { 
-              resolve(res);
-            })
-            .catch( (err) => {
-              reject(err);
-            });
-        });
-
-        return promise;
-      }
-
       const parseFromAPI = (res: any) => {
         let promise = new Promise( (resolve, reject) => {
 
+          // If database is empty parse initial data from Firebase
           if(res.rows.length === 0){
+
+            this.initial_database_loader = this.loadingCtrl.create({
+              content: "Please wait...",
+            });
+
+            this.initial_database_loader.present();
+
             this.apiSettings.getStudents()
-              .then( students => {
-                resolve(students); 
-              })
-              .catch( (err) => {
-                reject(err);
-              });
+              .then( students => resolve(students) )
+              .catch( (err) => reject(err) )
+          // When not empty try to seek for any update/change from Firebase
           } else {
             resolve(this.DO_NOTHING_HERE);
           }
@@ -82,39 +134,22 @@ export class SyncListPage {
         return promise;
       }
 
-      const syncOnce = (res: any) => {
-          let promises = [];
-          let query = `INSERT INTO students VALUES (?, ?, ?, ?, ?, ?)`;
-
-          _.map(res, (student, key) => {
-              promises.push( 
-                this.syncSettings.queryBuilder(query, 
-                  [
-                    student.name,
-                    student.age,
-                    student.information,
-                    student.level,
-                    student.created_at,
-                    student.updated_at,
-                  ])
-                );
-          });
-
-        return Promise.all(promises);
-      }
-
-      checkEmptyDatabase()
+      this.checkEmptyDatabase()
         .then(parseFromAPI)
-        .then(syncOnce)
+        .then(this.multiple_db_insert)
         .then((res: Array<any>) => {
+
           // Initial fill to database done.
           if(res.length === 0){
-            console.log('Already filled.');
-          } else {
-            console.log('Initial filled.');
-          }
 
-          this.getAllStudents();
+            this.checkDBupdates();
+
+          } else {
+            // NOTICE: when firstime database created.
+            this.initial_database_loader.dismiss();
+
+            this.populateStudents();
+          }
 
         })
         .catch((err)=> {
@@ -122,12 +157,117 @@ export class SyncListPage {
         });
   }
 
-  getAllStudents(){
+  checkDBupdates(){
+    /* 
+    | At this point, probably we have data already
+    | This method tried to seek any updates from the server
+    | using the last updated_at value
+    */
+    this.checkEmptyDatabase()
+    .then( (res: any) => {
+      if(res.rows.length !== 0){
+
+        this.getStudents()
+
+        .then( (res: any) => {
+
+          this.latestTimeRef = parseInt(res.rows.item(0).updated_at);
+
+          this.apiSettings 
+
+            .checkUpdates(this.latestTimeRef)
+
+              .then( (res) => {
+                  
+                  if( ! _.isEmpty(res)){
+
+                    this.updateLocalDatabase(res);
+
+                  } else {
+
+                    this.populateStudents();
+
+                    this.presentToast('Everything is up to date.');
+
+                    if(this.refresherRef){
+                      this.refresherRef.complete();
+                      this.refresherRef = null;
+                    }
+
+                  }
+              })
+              .catch( (err) => {
+                alert('SYNC-LIST.ts checkUpdates ERR ' + err);
+              });
+        })
+        .catch( (err) => {
+          alert('SNYC-LIST.ts getStudents ERR ' + JSON.stringify(err));
+        });
+      }
+    })
+    .catch( (err) => {
+      alert('SYNC-LIST.ts checkEmptyDatabase ERR ' + JSON.stringify(err));
+    });
+
+  }
+
+  updateLocalDatabase(res){
+    let loader = this.loadingCtrl.create({
+      content: "Preparing for updates...",
+    });
+
+    loader.present();
+
+    this.multiple_db_insert(res)
+      .then( (res) => {
+          
+          loader.dismiss();
+
+          this.populateStudents();
+
+          if(this.refresherRef){
+            this.refresherRef.complete();
+            this.refresherRef = null;
+          }
+      })
+      .catch( (err) => {
+        alert('SYNC-LIST.ts updateLocalDatabase ERR ' + JSON.stringify(err));
+      });
+  }
+
+  presentToast(message){
+    let toast = this.toastCtrl.create({
+      message: message,
+      duration: 3000
+    });
+    toast.present();
+  }
+
+  getStudents(){
     let collections = [];
     let query = `SELECT rowid, name, age, information, level, created_at, updated_at
                 FROM students
+                ORDER BY updated_at 
+                DESC
                 `;
-    this.syncSettings.queryBuilder(query, [])
+
+    let promise = new Promise( (resolve, reject) => {
+        this.syncSettings.queryBuilder(query, [])
+        .then( (res: any) => {
+          resolve(res);
+        })
+        .catch( (err) => {
+          reject(err);
+        });
+    });
+
+    return promise;
+  }
+
+  populateStudents(){
+    let collections = [];
+
+    this.getStudents()
       .then( (res: any) => {
         for(let i = 0; i < res.rows.length; i++){
             collections.push({
@@ -146,11 +286,12 @@ export class SyncListPage {
         }
       })
       .catch( (err) => {
-        console.log('Sync-list.ts POPULATE ERROR')
+        console.log('Sync-list.ts POPULATE ERROR');
       });
   }
   
   refreshAll(refresher){
-    
+    this.refresherRef = refresher;
+    this.checkDBupdates();
   }
 }
--- src/shared/api-settings.service.ts
+++ src/shared/api-settings.service.ts
@@ -2,23 +2,67 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import 'rxjs';
 import { Observable } from 'rxjs/Observable';
+import * as firebase from 'firebase';
 
 @Injectable()
 
 export class ApiSettings {
 
+    firebase_api;
+
     private baseUrl = 'https://prototypesyncmodule.firebaseio.com';
 
     constructor(private http: Http) {
-        //
+        let config = {
+            apiKey: "AIzaSyAAws_4wBh84H4wK24On__b66q3Qx9iSHM",
+            authDomain: "prototypesyncmodule.firebaseapp.com",
+            databaseURL: "https://prototypesyncmodule.firebaseio.com",
+            projectId: "prototypesyncmodule",
+            storageBucket: "prototypesyncmodule.appspot.com",
+            messagingSenderId: "432874785744"
+        };
+        this.firebase_api = firebase.initializeApp(config);
     }
 
     getStudents(){
         return new Promise( (resolve, reject) => {
-            this.http.get(`${this.baseUrl}/students.json`)
-                     .subscribe( (res) => {
-                        resolve(res.json());
-                     });
+            this.firebase_api.database().ref('/students')
+            .orderByChild('inverted_ts')
+            .limitToLast(100) // TODO: pagination
+            .once('value', 
+                (snap) => {
+                    let fixedOrder = {};
+                    snap.forEach( (child) => {
+                        fixedOrder[child.key] = child.val();
+                    });
+                    resolve(fixedOrder);
+                },
+                (err) => {
+                    reject(err);
+                });
+        });
+    }
+
+    checkUpdates(timestamp){
+        return new Promise( (resolve, reject) => {
+            let end = Date.now();
+            let start   = timestamp + 1;
+            
+            this.firebase_api.database().ref('/students')
+            .orderByChild('updated_at')
+            .startAt(start)
+            .endAt(end)
+            .once('value', 
+                (snap) => {
+                    let fixedOrder = {};
+                    snap.forEach( (child) => {
+                        fixedOrder[child.key] = child.val();
+                    });
+                    resolve(fixedOrder);
+                },
+                (err) => {
+                    reject(err);
+                });
         });
     }
 }
